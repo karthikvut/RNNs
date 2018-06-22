62346cf3955127c3e46e27218879d6d527ed5c95
karthikvut <karthikjobmail1@gmail.com>
2018-06-22 13:52:58 -0500

    Create rnnlm.py

---
+++ rnnlm.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+from preprocessing import getSentenceData
+from RNN import Model
+
+word_dim = 8000
+hidden_dim = 100
+
+X_train, y_train = getSentenceData('data/reddit-comments.csv',word_dim)
+np.random.seed(10)
+rnn = Model(word_dim,hidden_dim)
+
+losses = rnn.train(X_train[:100], y_train[:100], learning_rate=0.005, nepoch=10, evaluate_loss_after=1)
